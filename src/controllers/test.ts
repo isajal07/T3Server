8d66950e33c5bf1d9c77092ae24baedb00a7568d
Sajal Shrestha <sajal@Sajals-MacBook-Pro.local>
2023-03-02 16:47:34 -0800

    wip

---
+++ src/controllers/test.ts
@@ -0,0 +1,594 @@
+const ress = {
+    "name": "sjl",
+    "role": 0,
+    "teammateSpecies": 0,
+    "dayAndTime": "3/2/2023 11:13:40 PM",
+    "gameDifficulty": 0,
+    "whitehatScore": 900.0,
+    "blackhatScore": 2300.0,
+    "records": [
+      {
+        "secondsFromStart": 0.0,
+        "eventName": "StartWave",
+        "eventModifiers": [
+          ""
+        ]
+      },
+      {
+        "secondsFromStart": 5.601127624511719,
+        "eventName": "SetNewMaliciousRule",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 5.601127624511719,
+        "eventName": "SetNewMaliciousRule",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 11.639999389648438,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 13.199999809265137,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 14.199999809265137,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 14.526839256286621,
+        "eventName": "MaliciousDestinationClicked",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 15.21323013305664,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI"
+        ]
+      },
+      {
+        "secondsFromStart": 15.21323013305664,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI, MaliciousDestinationClicked latency, 0.69"
+        ]
+      },
+      {
+        "secondsFromStart": 16.388830184936525,
+        "eventName": "PacketInspect",
+        "eventModifiers": [
+          "Small, Pink, Sphere, North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 16.979999542236329,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 17.979999542236329,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 18.979999542236329,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 19.21378517150879,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ""
+        ]
+      },
+      {
+        "secondsFromStart": 19.21378517150879,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ", RuleSpec latency, 19.21"
+        ]
+      },
+      {
+        "secondsFromStart": 19.980911254882814,
+        "eventName": "AdvisedFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", PickedAdvisorType latency, 4.77"
+        ]
+      },
+      {
+        "secondsFromStart": 19.980911254882814,
+        "eventName": "AdvisedFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", AdviceAppeared latency, 0.77"
+        ]
+      },
+      {
+        "secondsFromStart": 21.237049102783204,
+        "eventName": "MaliciousDestinationClicked",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 21.868934631347658,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI"
+        ]
+      },
+      {
+        "secondsFromStart": 21.868934631347658,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI, MaliciousDestinationClicked latency, 0.63"
+        ]
+      },
+      {
+        "secondsFromStart": 23.31688117980957,
+        "eventName": "PacketInspect",
+        "eventModifiers": [
+          "Large, Blue, Capsule, South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 23.65999984741211,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 23.979999542236329,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 24.30865478515625,
+        "eventName": "RuleSpec",
+        "eventModifiers": [
+          "Large"
+        ]
+      },
+      {
+        "secondsFromStart": 24.65999984741211,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 24.684429168701173,
+        "eventName": "RuleSpec",
+        "eventModifiers": [
+          "Blue"
+        ]
+      },
+      {
+        "secondsFromStart": 25.17330551147461,
+        "eventName": "RuleSpec",
+        "eventModifiers": [
+          "Capsule"
+        ]
+      },
+      {
+        "secondsFromStart": 25.60441780090332,
+        "eventName": "UserBuiltFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", PickedAdvisorType latency, 3.74"
+        ]
+      },
+      {
+        "secondsFromStart": 25.60441780090332,
+        "eventName": "UserBuiltFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", AdviceAppeared latency, 6.39"
+        ]
+      },
+      {
+        "secondsFromStart": 25.869356155395509,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ""
+        ]
+      },
+      {
+        "secondsFromStart": 25.869356155395509,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ", RuleSpec latency, 25.87"
+        ]
+      },
+      {
+        "secondsFromStart": 26.0,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 26.719999313354493,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 27.0,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 29.719999313354493,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 30.600038528442384,
+        "eventName": "StartWave",
+        "eventModifiers": [
+          ""
+        ]
+      },
+      {
+        "secondsFromStart": 32.0,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 36.200538635253909,
+        "eventName": "SetNewMaliciousRule",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 36.200538635253909,
+        "eventName": "SetNewMaliciousRule",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 38.10099411010742,
+        "eventName": "MaliciousDestinationClicked",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 38.88420867919922,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI"
+        ]
+      },
+      {
+        "secondsFromStart": 38.88420867919922,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI, MaliciousDestinationClicked latency, 0.78"
+        ]
+      },
+      {
+        "secondsFromStart": 42.31999969482422,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 42.8848762512207,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ""
+        ]
+      },
+      {
+        "secondsFromStart": 42.8848762512207,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ", RuleSpec latency, 42.88"
+        ]
+      },
+      {
+        "secondsFromStart": 42.98896026611328,
+        "eventName": "BackButtonNoFirewallSet",
+        "eventModifiers": [
+          ", PickedAdvisorType latency, 4.10"
+        ]
+      },
+      {
+        "secondsFromStart": 42.98896026611328,
+        "eventName": "BackButtonNoFirewallSet",
+        "eventModifiers": [
+          ", AdviceAppeared latency, 0.10"
+        ]
+      },
+      {
+        "secondsFromStart": 44.01297378540039,
+        "eventName": "MaliciousDestinationClicked",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 44.29999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 44.79999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 45.05327224731445,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "Human"
+        ]
+      },
+      {
+        "secondsFromStart": 45.05327224731445,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "Human, MaliciousDestinationClicked latency, 1.04"
+        ]
+      },
+      {
+        "secondsFromStart": 45.31999969482422,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 46.14944839477539,
+        "eventName": "PacketInspect",
+        "eventModifiers": [
+          "Medium, Pink, Sphere, South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 47.29999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 47.63999938964844,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 48.00510787963867,
+        "eventName": "RuleSpec",
+        "eventModifiers": [
+          "Medium"
+        ]
+      },
+      {
+        "secondsFromStart": 48.29999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 48.46877670288086,
+        "eventName": "RuleSpec",
+        "eventModifiers": [
+          "Pink"
+        ]
+      },
+      {
+        "secondsFromStart": 48.79999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 49.05400848388672,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ""
+        ]
+      },
+      {
+        "secondsFromStart": 49.05400848388672,
+        "eventName": "AdviceAppeared",
+        "eventModifiers": [
+          ", RuleSpec latency, 49.05"
+        ]
+      },
+      {
+        "secondsFromStart": 49.31999969482422,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 49.348365783691409,
+        "eventName": "RuleSpec",
+        "eventModifiers": [
+          "Sphere"
+        ]
+      },
+      {
+        "secondsFromStart": 50.29999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 50.324790954589847,
+        "eventName": "AdvisedFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", PickedAdvisorType latency, 5.27"
+        ]
+      },
+      {
+        "secondsFromStart": 50.324790954589847,
+        "eventName": "AdvisedFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", AdviceAppeared latency, 1.27"
+        ]
+      },
+      {
+        "secondsFromStart": 50.79999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 51.44468307495117,
+        "eventName": "MaliciousDestinationClicked",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 52.06090545654297,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI"
+        ]
+      },
+      {
+        "secondsFromStart": 52.06090545654297,
+        "eventName": "PickedAdvisorType",
+        "eventModifiers": [
+          "AI, MaliciousDestinationClicked latency, 0.62"
+        ]
+      },
+      {
+        "secondsFromStart": 52.31999969482422,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 52.79999923706055,
+        "eventName": "MaliciousPacketUnfiltered_BadForUs",
+        "eventModifiers": [
+          "North East Side"
+        ]
+      },
+      {
+        "secondsFromStart": 53.63999938964844,
+        "eventName": "MaliciousPacketFiltered_GoodForUs",
+        "eventModifiers": [
+          "South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 53.88436508178711,
+        "eventName": "PacketInspect",
+        "eventModifiers": [
+          "Medium, Pink, Sphere, South West Side"
+        ]
+      },
+      {
+        "secondsFromStart": 55.69260787963867,
+        "eventName": "UserBuiltFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", PickedAdvisorType latency, 3.63"
+        ]
+      },
+      {
+        "secondsFromStart": 55.69260787963867,
+        "eventName": "UserBuiltFirewallCorrectAndSet",
+        "eventModifiers": [
+          ", AdviceAppeared latency, 6.64"
+        ]
+      },
+    ]
+  }
+  
+  let { name, dayAndTime, whitehatScore, blackhatScore, records } = ress;
+  
+  //Split eventModifier
+  records = records.map(rec => {
+    return {
+      ...rec,
+      eventModifiers: rec.eventModifiers[0].split(',')
+    }
+  });
+  
+  const events = records.map(rec => {
+    //Add bulding enum
+    const build = rec.eventModifiers.includes("North East Side") || rec.eventModifiers.includes("South West Side");
+    const buildingEnum = build && rec.eventModifiers[0] === "North East Side" ? 1 : 2;
+  
+    //Add advisor
+    const advisorExist = rec.eventModifiers[0] === "Human" || rec.eventModifiers[0] === "AI";
+    const advisorEnum = advisorExist && rec.eventModifiers[0] === "AI" ? 1 : 2;
+  
+    //Add rule
+    const ruleExist =
+      (rec.eventModifiers[0] === 'Small' ||
+        rec.eventModifiers[0] === 'Medium' ||
+        rec.eventModifiers[0] === 'Large');
+    const rules = ruleExist && rec.eventModifiers.slice(0, 3).toString();
+    const buildingRules = ruleExist && rec.eventModifiers[3];
+    
+    //Add latency
+    const latencyExist = rec.eventModifiers.length === 3 && rec.eventModifiers[1].includes('latency');
+    
+    return {
+      time: rec.secondsFromStart,
+      eventName: latencyExist ?  rec.eventModifiers[1]: rec.eventName,
+      ...(build) && { building: buildingEnum },
+      ...(advisorExist) && { advisor: advisorEnum },
+      ...(ruleExist) && { rules: rules },
+      ...(ruleExist) && { building: buildingRules },
+      ...(latencyExist) && { latency: rec.eventModifiers[2]}
+    }
+  })
+  
+  console.log(events)
+  
