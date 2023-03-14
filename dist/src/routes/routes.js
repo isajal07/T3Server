25f6714c14e2894fe4a782d3b83e444cae17b215
Sajal Shrestha <sajal@Sajals-MacBook-Pro.local>
2023-03-13 23:11:31 -0700

    feat: vercel config

--- dist/src/routes/routes.js
+++ dist/src/routes/routes.js
@@ -55,4 +55,5 @@ POST("/createUserGameData", userGameDataController.createUserGameData);
 GET("/getUserGameDatas", userGameDataController.getUserGameDatas);
 GET("/getUserGameData/:userGameDataId", userGameDataController.getUserGameData);
 GET("/getUserGameData/studyId/:studyId", userGameDataController.getUserGameDataByStudyId);
+GET("/sajal", userGameDataController.sajalTest);
 module.exports = router;
